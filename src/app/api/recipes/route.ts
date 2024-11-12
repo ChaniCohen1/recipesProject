25b716d6602de976686757e2121e018765d1b535
Chani Cohen <72072ch@gmail.com>
2024-11-12 11:22:37 +0200

    crud server

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -1,5 +1,5 @@
-import connect from "@/app/lib/DB/mongoDB";
-import Todo from "@/app/lib/models/Todo";
+import connect from "@/app/lib/DB/connectDB";
+import Recipe from "@/app/lib/models/recipe";
 import { NextResponse } from "next/server";
 
 
@@ -7,15 +7,20 @@ export async function POST(request: Request) {
     try {
 
         await connect();
-        console.log("before");
-        const newTodo = new Todo({
-            todo: "משימה חדשה",           
-            completed: true,    
+        const {mealName, category, PreparationInstructions, ingredients, isFavorite, image} = await request.json();
+
+        const newRecipe = new Recipe({
+            mealName: mealName,
+            category: category,
+            PreparationInstructions: PreparationInstructions,
+            ingredients: ingredients,
+            isFavorite: isFavorite,
+            image: image
         });
         
-        await newTodo.save();
+        await newRecipe.save();
         
-        return NextResponse.json({ todo: newTodo}, { status: 200 });
+        return NextResponse.json({ recipe: newRecipe}, { status: 200 });
     } catch(error) {
         console.error(error);
         return NextResponse.json({ massage: 'Error: failed to crate todo' }, { status: 500 });
@@ -45,16 +50,14 @@ export async function GET(request: Request) {
     try {
 
         await connect();
-
-        
-        const todos = await Todo.find();
-        if (todos)
-            return NextResponse.json(todos, { status: 200 });
+        const recipes = await Recipe.find();
+        if (recipes)
+            return NextResponse.json(recipes, { status: 200 });
         else
             return NextResponse.json({ error: "not found" }, { status: 500 });
 
     } catch {
-        return NextResponse.json({ error: "failed to get todo" }, { status: 500 });
+        return NextResponse.json({ error: "failed to get recipe" }, { status: 500 });
     }
 };
 
@@ -64,18 +67,19 @@ export async function PUT(request: Request) {
 
         const url = new URL(request.url);
         const id = url.searchParams.get('id');
-        const { todo, completed } = await request.json();
-        await connect();
-        console.log("before", todo, completed);
 
-        const updatedTodo = await Todo.findByIdAndUpdate(
+        const {mealName, category, PreparationInstructions, ingredients, isFavorite, image} = await request.json();
+
+
+        await connect();
+
+        const updatedRecipe = await Recipe.findByIdAndUpdate(
             id,
-            { $set: { todo, completed } },
+            { $set: {mealName, category, PreparationInstructions, ingredients, isFavorite, image} },
             { new: true }
         );
-        console.log("after");
 
-        return NextResponse.json({ updatedTodo: updatedTodo }, { status: 200 });
+        return NextResponse.json({ updatedTodo: updatedRecipe }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error: error }, { status: 500 });
     }
@@ -89,7 +93,7 @@ export async function DELETE(request: Request) {
 
         await connect();
 
-        await Todo.findByIdAndDelete(id);
+        await Recipe.findByIdAndDelete(id);
         return NextResponse.json({ massage: `object deleted` }, { status: 200 });
     }
     catch (error) {
