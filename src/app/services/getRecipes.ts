2f649a1155d6a4d8c0b6e770787b90cbc283ca2f
Chani Cohen <72072ch@gmail.com>
2024-11-13 11:39:39 +0200

    add var url

--- src/app/services/getRecipes.ts
+++ src/app/services/getRecipes.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import Recipe from "../types/recipes";
 
+const url = "http://recipes-project-omega.vercel.app";
 
 export const addRecipe = async (newRecipe: Recipe) => {
     try{
-        const response = await axios.post('http://recipes-project-omega.vercel.app/api/recipes', newRecipe);
+        const response = await axios.post(`${url}/api/recipes`, newRecipe);
         return response.data;
     }catch(error){
         console.error('Error adding recipe:', error);
@@ -14,7 +15,7 @@ export const addRecipe = async (newRecipe: Recipe) => {
 
 export const getRecipes = async () => {    
     try{
-        const response = await axios.get('https://recipes-project-omega.vercel.app/api/recipes');
+        const response = await axios.get(`${url}/api/recipes`);
         
         return response.data;
     }catch(error){
@@ -25,7 +26,7 @@ export const getRecipes = async () => {
 
 export const getRecipeById = async (id: string) => {
     try{
-        const response = await axios.get(`https://recipes-project-omega.vercel.app/api/recipes/${id}`);
+        const response = await axios.get(`${url}/api/recipes/${id}`);
         return response.data;
     }catch(error){
         console.error('error get recipes', error);
@@ -35,7 +36,7 @@ export const getRecipeById = async (id: string) => {
 
 export const editRecipe = async (id: string, editRecipe: Recipe) => {
     try{
-        const response = await axios.put(`https://recipes-project-omega.vercel.app/api/recipes/${id}`, editRecipe);
+        const response = await axios.put(`${url}/api/recipes/${id}`, editRecipe);
         return response.data;
     }catch(error){
         console.error('error edit recipe', error);
@@ -45,7 +46,7 @@ export const editRecipe = async (id: string, editRecipe: Recipe) => {
 
 export const deleteRecipe = async (id: string) => {
     try{
-        const response = await axios.delete(`https://recipes-project-omega.vercel.app/api/recipes/${id}`);
+        const response = await axios.delete(`${url}/api/recipes/${id}`);
         return response.data;
     }catch(error){
         console.error('error edit recipe', error);
