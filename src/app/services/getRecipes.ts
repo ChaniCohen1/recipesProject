9f778feef116980a5ab0fcefea9e632d1068ae1f
YaelNagar <165303327+YaelNagar@users.noreply.github.com>
2024-11-14 11:10:04 +0200

    local storage update before component

--- src/app/services/getRecipes.ts
+++ src/app/services/getRecipes.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Recipe from "../types/recipes";
 
-const url = "http://localhost:3000";
+const url = "https://recipes-project-omega.vercel.app";
 
 export const addRecipe = async (newRecipe: Recipe) => {
     try{
