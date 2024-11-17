2c8f0a8fc6037bb9ecdb15c85120e624eacd1b3d
Chani Cohen <72072ch@gmail.com>
2024-11-17 12:41:35 +0200

    add router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 
+import { useRouter } from "next/navigation"
+
 export default function Home() {
   
+  const router = useRouter();
+  router.push('/pages/login');
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      welcome
     </main>
   )
 }
